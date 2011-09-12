58fd4612244e3a10b2873f900c03a2686e4570b5
Edward Bridges <github@eqbridges.com>
2011-09-12 10:30:53 +0000

    tweaked application name

--- src/main/java/tinfoil/Constants.java
+++ src/main/java/tinfoil/Constants.java
@@ -17,15 +17,15 @@ public class Constants {
         MIMETYPE_MAP.addMimeTypes("image/gif gif");
     }
 
-    public static final String APPLICATION_VENDOR = "tinfoil";
-    public static final String APPLICATION_NAME = "picasa-uploader";
+    public static final String APPLICATION_VENDOR = "Tinfoil";
+    public static final String APPLICATION_NAME = "PicasaUploader";
     public static final String APPLICATION_VERSION = "0.2";
-    public static final String APPLICATION_AGENT = String.format("%s/%s/v.%s",APPLICATION_VENDOR,APPLICATION_NAME,APPLICATION_VERSION);
+    public static final String APPLICATION_AGENT = String.format("%s-%s-%s",APPLICATION_VENDOR,APPLICATION_NAME,APPLICATION_VERSION);
 
-    public static final String PHOTO_DIRECTORY = "/Users/ebridges/Pictures/picasa-upload";
     public static final String DEFAULT_ACCESS = "private";
 
-
+    
+    public static final String PHOTO_DIRECTORY = "/Users/ebridges/Pictures/picasa-upload";
     public static final String DIGIKAM_DATABASE_DRIVER = "org.sqlite.JDBC.class";
     public static final String DIGIKAM_DATABASE_URL = "jdbc:sqlite:/"+PHOTO_DIRECTORY+"/digikam.db";
 
